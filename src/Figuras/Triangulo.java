2ffdd459f7e785ce3d7cb3134bbf36ec8b37689e
PC <jorgerbf22@gmail.com>
2022-09-07 09:39:02 -0500

    Subiendo

---
+++ src/Figuras/Triangulo.java
@@ -0,0 +1,11 @@
+package Figuras;
+
+public class Triangulo {
+    public double lado3;
+    public  double altura;
+    public  double area;
+    public double lado1;
+    public double lado2;
+
+
+}
